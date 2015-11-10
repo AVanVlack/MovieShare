63f6faae9ccd476fcc2e466b905346cea33c4d18
Andrew VanVlack <A.VanVlack@Gmail.com>
2015-11-10 01:35:45 -0800

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('movieSyncApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
